c3f7f776ff2af159b9833c84a289ac89c25e2145
NewUser <you@example.com>
2025-07-15 00:41:04 +0300

    clean github trash

--- image-loader.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = ({ src }) => {
-  const assetPrefix = 'https://teploniweta.github.io/testNextWrk'
-  // src будет иметь вид /images/foo.png, поэтому мы просто добавляем префикс.
-  return `${assetPrefix}${src}`
-} 
--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-const repo = 'testNextWrk'
-const assetPrefix = isProd ? `https://teploniweta.github.io/${repo}` : undefined
-const basePath = isProd ? `/${repo}` : undefined
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true,
-  images: isProd
-    ? {
-        loader: 'custom',
-        loaderFile: './image-loader.js',
-      }
-    : {
-        unoptimized: true,
-      },
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig 
